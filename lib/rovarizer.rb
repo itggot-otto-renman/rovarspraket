13296b9b0ea587d7b551617405c34061aeadb4b2
itggot-otto-renman <otto.renman@itggot.se>
2016-12-07 10:01:19 +0100

    Blev klar med koden
    läs titeln

---
+++ lib/rovarizer.rb
@@ -0,0 +1,17 @@
+def rovarize(sentance:)
+
+vowels = ["A", "E", "I", "O", "U", "Y", "Å", "Ä", "Ö"]
+output = ""
+index = 0
+  while index < sentance.length
+    letter = sentance[index]
+    if vowels.include?(letter.upcase)
+      output = output + letter
+    else
+      output = output + letter + "o" + letter
+    end
+    index = index + 1
+  end
+  return output
+
+end
